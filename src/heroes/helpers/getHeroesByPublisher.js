f4190fdd359f51b97e4399a4b4d08e3998ad25ec
Fabio de Oliveira <fdeoliveira@EN2110479.local>
2024-06-20 14:03:54 -0300

    init

---
+++ src/heroes/helpers/getHeroesByPublisher.js
@@ -0,0 +1,11 @@
+import { heroes } from '../data/heroes'
+
+export const getHeroesByPublisher = (publisher) => {
+
+    const validPublisher = ['DC Comics', 'Marvel Comics'];
+    if (!validPublisher.includes(publisher)) {
+        throw new Error(`${publisher} don't mach`)
+    }
+    return heroes.filter(heroe => heroe.publisher == publisher);
+
+}
